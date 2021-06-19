b4e7fb175190121f62a5a06b5947d651f1706c9b
Joey <30815220+joeylking@users.noreply.github.com>
2021-06-19 12:36:02 -0500

    Rolling 1

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ const rollBtn = document.querySelector('.btn--roll');
 const newBtn = document.querySelector('.btn--new');
 const holdBtn = document.querySelector('.btn--hold');
 const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 rollBtn.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6 + 1);
@@ -24,6 +27,13 @@ rollBtn.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
 });
